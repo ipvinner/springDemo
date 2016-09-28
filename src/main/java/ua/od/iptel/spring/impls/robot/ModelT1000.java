bbf44ce99c6db8982afaef9b88b5c67e850940bf
ipvinner <vaniavinogradov@rambler.ru>
2016-09-28 20:08:41 +0300

    robot added

--- src/main/java/ua/od/iptel/spring/impls/robot/ModelT1000.java
+++ src/main/java/ua/od/iptel/spring/impls/robot/ModelT1000.java
@@ -11,6 +11,60 @@ public class ModelT1000 implements Robot {
 	private Leg leg;
 	private Head head;
 
+	private String color;
+	private int year;
+	private boolean sound;
+
+	public ModelT1000() {
+	}
+
+	public ModelT1000(Hand hand, Leg leg, Head head) {
+		super();
+		this.hand = hand;
+		this.leg = leg;
+		this.head = head;
+	}
+
+	public ModelT1000(String color, int year, boolean sound) {
+		this.color = color;
+		this.year = year;
+		this.sound = sound;
+	}
+
+	public ModelT1000(Hand hand, Leg leg, Head head, String color, int year, boolean sound) {
+		super();
+		this.hand = hand;
+		this.leg = leg;
+		this.head = head;
+		this.color = color;
+		this.year = year;
+		this.sound = sound;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public boolean isSound() {
+		return sound;
+	}
+
+	public void setSound(boolean sound) {
+		this.sound = sound;
+	}
+
 	public Hand getHand() {
 		return hand;
 	}
@@ -35,21 +89,16 @@ public class ModelT1000 implements Robot {
 		this.head = head;
 	}
 
-	public ModelT1000() {
-	}
 
-	public ModelT1000(Hand hand, Leg leg, Head head) {
-		super();
-		this.hand = hand;
-		this.leg = leg;
-		this.head = head;
-	}
 
 	@Override
 	public void action() {
 		head.calc();
 		hand.catchSomething();
 		leg.go();
+		System.out.println("color " + this.color);
+		System.out.println("year " + this.year);
+		System.out.println("can play sound " + this.sound);
 	}
 
 	@Override
